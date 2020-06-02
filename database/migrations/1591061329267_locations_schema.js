f9306223db99ce673fa40e2c22a140e828a39c20
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2020-06-02 00:50:49 -0300

    create tables for project's entities.

---
+++ database/migrations/1591061329267_locations_schema.js
@@ -0,0 +1,40 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class LocationsSchema extends Schema {
+  up () {
+    this.create('locations', (table) => {
+      table.increments()
+      table.decimal('lat').notNullable()
+      table.decimal('lng').notNullable()
+      table.string('name', 254).notNullable()
+      table.string('street', 254).notNullable()
+      table.string('street_number', 10)
+      table.string('complement', 100)
+      table.string('district', 50).notNullable()
+      table.string('city', 50).notNullable()
+      table.string('state', 50).notNullable()
+      table.string('country', 100).notNullable()
+      table.string('zipcode', 15)
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+      table.unique(['lat', 'lng'])
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('locations')
+  }
+}
+
+module.exports = LocationsSchema
---
+++ database/migrations/1591062449128_ratings_schema.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class RatingsSchema extends Schema {
+  up () {
+    this.create('ratings', (table) => {
+      table.increments()
+      table.integer('rate').notNullable()
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table
+        .integer('location_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('locations')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+      table.unique(['user_id', 'location_id'])
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('ratings')
+  }
+}
+
+module.exports = RatingsSchema
